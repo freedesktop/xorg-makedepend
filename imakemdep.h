020b6920beeebd40cab7e586c548257a3eb16473
Egbert Eich <eich@suse.de>
2004-03-02 14:16:07 +0000

    Removing last bits of kdrive. 10. Made use system supplied Expat and Freetype2 library the default on     Linux (documented in the release notes) (Bugzilla #218,#219) (Egbert     Eich). 9. Patched __AMD64__ flags to __amd64__ (Egbert Eich). 8. Added RELNOTES.XORG to collect bits for release notes for this release     (Egbert Eich). 7. Removed leftovers from libxml2 (Bugzilla #220)(Egbert Eich).

--- imakemdep.h
+++ imakemdep.h
@@ -371,11 +371,11 @@ char *cpp_argv[ARGUMENTS] = {
 # ifdef __ia64__
 	"-D__ia64__",
 # endif
-# ifdef __AMD64__
-	"-D__AMD64__",
+# ifdef __amd64__
+	"-D__amd64__",
 # endif
 # ifdef __x86_64__
-	"-D__AMD64__",
+	"-D__amd64__",
 # endif
 # ifdef __s390__
 	"-D__s390__",
@@ -1264,12 +1264,12 @@ struct symtab	predefs[] = {
 # ifdef __ia64__
 	{"__ia64__", "1"},
 # endif
-# if defined (AMD64) || defined (x86_64)
-	{"AMD64", "1"},
+# if defined (amd64) || defined (x86_64)
+	{"amd64", "1"},
 	{"x86_64", "1"},
 # endif
-# if defined (__AMD64__) || defined (__x86_64__)
-	{"__AMD64__", "1"},
+# if defined (__amd64__) || defined (__x86_64__)
+	{"__amd64__", "1"},
 	{"__x86_64__", "1"},
 # endif
 # ifdef __i386
