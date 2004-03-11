60665fba7faf87956929cfb29709c0af2ad2aa2f
Mike A. Harris <mharris@redhat.com>
2004-03-11 09:16:51 +0000

    Added initial support for PowerPC 64 (ppc64) architecture. (Mike A. Harris,     Elliot Lee, SuSE)

--- imakemdep.h
+++ imakemdep.h
@@ -618,6 +618,9 @@ char *cpp_argv[ARGUMENTS] = {
 # ifdef ppc
 	"-Dppc",
 # endif
+# ifdef ppc64
+	"-Dppc64",
+# endif
 # if defined(m68k)  || defined(M68k) || defined(m68040)
 	"-Dm68k",
 	"-DM68k",
@@ -677,6 +680,10 @@ char *cpp_argv[ARGUMENTS] = {
 	"-DSVR4",
 # endif
 # ifdef __powerpc__
+#  ifdef __powerpc64__
+	"-D__powerpc64__",
+#  endif
+# else
 	"-D__powerpc__",
 # endif
 # ifdef PowerMAX_OS
@@ -710,6 +717,9 @@ char *cpp_argv[ARGUMENTS] = {
         "-D__DARWIN__",
 # ifdef __ppc__
         "-D__ppc__",
+# endif
+# ifdef __ppc64__
+        "-D__ppc64__",
 # endif
 # ifdef __i386__
         "-D__i386__",
@@ -1253,6 +1263,10 @@ struct symtab	predefs[] = {
         {"__QNXNTO__", "1"},
 #endif
 # ifdef __powerpc__
+#  ifdef __powerpc64__
+	{"__powerpc64__", "1"},
+#  endif
+# else
 	{"__powerpc__", "1"},
 # endif
 # ifdef PowerMAX_OS
@@ -1331,6 +1345,9 @@ struct symtab	predefs[] = {
 # endif
 #if defined(__ppc__)
         {"__ppc__", "1"},
+#endif
+#if defined(__ppc64__)
+        {"__ppc64__", "1"},
 #endif
 #if defined(__BIG_ENDIAN__)
       {"__BIG_ENDIAN__", "1"},
